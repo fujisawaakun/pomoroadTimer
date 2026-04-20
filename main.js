5b391ebb89205332204d5e8d645f925d517ab3a6
fujisawaakun <132373009+fujisawaakun@users.noreply.github.com>
2026-04-20 22:17:57 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,15 +7,27 @@
   const minutesInput = document.getElementById("minutes-input");
   const secondsInput = document.getElementById("seconds-input");
 
+  const hoursInterval = document.getElementById("hours-interval");
+  const minutesInterval = document.getElementById("minutes-interval");
+  const secondsInterval = document.getElementById("seconds-interval");
+
   const settingDisplay = document.getElementById("setting-display");
   const settingIcon = document.getElementById("setting-icon");
 
   let intervalId;
 
-  function getTotalTime() {
+  function getWorkTotalTime() {
     const h = Number(hoursInput.value) || 0;
     const m = Number(minutesInput.value) || 0;
     const s = Number(secondsInput.value) || 0;
+
+    return (h * 3600 + m * 60 + s) * 1000;
+  }
+
+  function getRestTotalTime() {
+    const h = Number(hoursInterval.value) || 0;
+    const m = Number(minutesInterval.value) || 0;
+    const s = Number(secondsInterval.value) || 0;
 
     return (h * 3600 + m * 60 + s) * 1000;
   }
@@ -33,19 +45,42 @@
   }
 
   function previewTime() {
-    const totalTime = getTotalTime();
+    const totalTime = getWorkTotalTime();
     updateTimerText(totalTime);
   }
 
+  function activeInterval() {
+    const totalTime = getRestTotalTime();
+
+    if (totalTime <= 0) {
+      return;
+    }
+
+    const endTime = Date.now() + totalTime;
+    updateTimerText(totalTime);
+    intervalId = setInterval(() => {
+      const countDown = endTime - Date.now();
+
+      if (countDown <= 0) {
+        clearInterval(intervalId);
+        updateTimerText(0);
+        return;
+      }
+      updateTimerText(countDown);
+    }, 100);
+    return;
+  }
+
   start.addEventListener("click", () => {
-    const totalTime = getTotalTime();
+    const totalTime = getWorkTotalTime();
+
     if (totalTime <= 0) {
       return;
     }
 
     clearInterval(intervalId);
 
-    const endTime = Date.now() + getTotalTime();
+    const endTime = Date.now() + totalTime;
     updateTimerText(totalTime);
 
     intervalId = setInterval(() => {
@@ -54,6 +89,7 @@
       if (countDown <= 0) {
         clearInterval(intervalId);
         updateTimerText(0);
+        activeInterval();
         return;
       }
       updateTimerText(countDown);
