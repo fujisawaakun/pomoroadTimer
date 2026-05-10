042c90c944079a0ad89b18572a0f09e793d81878
fujisawaakun <132373009+fujisawaakun@users.noreply.github.com>
2026-05-10 23:34:08 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -66,34 +66,34 @@
     return Number(setCount.value) || 1;
   }
 
-  //受け取った数値を時・分・秒に分割、timerのテキストに表示
-  function updateTimerText(ms) {
-    const totalSec = Math.ceil(ms / 1000);
-    const hh = Math.floor(totalSec / 3600);
-    const mm = Math.floor((totalSec % 3600) / 60);
-    const ss = Math.floor(totalSec % 60);
+  // //受け取った数値を時・分・秒に分割、timerのテキストに表示
+  // function updateTimerText(ms) {
+  //   const totalSec = Math.ceil(ms / 1000);
+  //   const hh = Math.floor(totalSec / 3600);
+  //   const mm = Math.floor((totalSec % 3600) / 60);
+  //   const ss = Math.floor(totalSec % 60);
 
-    timer.textContent =
-      `${String(hh).padStart(1, "0")}:` +
-      `${String(mm).padStart(2, "0")}:` +
-      `${String(ss).padStart(2, "0")}`;
-  }
+  //   timer.textContent =
+  //     `${String(hh).padStart(1, "0")}:` +
+  //     `${String(mm).padStart(2, "0")}:` +
+  //     `${String(ss).padStart(2, "0")}`;
+  // }
 
-  function updateRing(remainingTime, totalTime) {
-    if (totalTime <= 0) {
-      progress.style.strokeDashoffset = circumference;
-      return;
-    }
+  // function updateRing(remainingTime, totalTime) {
+  //   if (totalTime <= 0) {
+  //     progress.style.strokeDashoffset = circumference;
+  //     return;
+  //   }
 
-    const ratio = remainingTime / totalTime;
-    const offset = circumference * (1 - ratio);
+  //   const ratio = remainingTime / totalTime;
+  //   const offset = circumference * (1 - ratio);
 
-    progress.style.strokeDashoffset = offset;
-  }
+  //   progress.style.strokeDashoffset = offset;
+  // }
 
   function previewTime() {
     const totalTime = getWorkTotalTime();
-    updateTimerText(totalTime);
+    updateTimerText(timer, totalTime);
     updateRing(totalTime, totalTime);
   }
 
@@ -110,7 +110,7 @@
     isRunning = true;
     updateStartIcon("pause");
 
-    updateTimerText(totalTime);
+    updateTimerText(timer, totalTime);
     updateRing(totalTime, totalTime);
 
     intervalId = setInterval(() => {
@@ -122,13 +122,13 @@
         remainingTime = 0;
         isRunning = false;
         updateStartIcon("play");
-        updateTimerText(0);
+        updateTimerText(timer, 0);
         updateRing(0, totalTime);
         finish();
         return;
       }
 
-      updateTimerText(countDown);
+      updateTimerText(timer, countDown);
       updateRing(countDown, totalTime);
     }, 45);
   }
@@ -181,7 +181,7 @@
         remainingTime = 0;
         isRunning = false;
         updateStartIcon("play");
-        updateTimerText(0);
+        updateTimerText(timer, 0);
         updateRing(0, totalTime);
 
         if (currentMode === "work") {
@@ -197,25 +197,25 @@
         return;
       }
 
-      updateTimerText(countDown);
+      updateTimerText(timer, countDown);
       updateRing(countDown, totalTime);
     }, 45);
   }
 
-  function updateStartIcon(date) {
-    if (date === "play") {
-      startIconPath.setAttribute("d", "M8 5v14l11-7z");
-    }
-    if (date === "pause") {
-      startIconPath.setAttribute("d", "M6 5h4v14H6zm8 0h4v14h-4z");
-    }
-  }
+  // function updateStartIcon(date) {
+  //   if (date === "play") {
+  //     startIconPath.setAttribute("d", "M8 5v14l11-7z");
+  //   }
+  //   if (date === "pause") {
+  //     startIconPath.setAttribute("d", "M6 5h4v14H6zm8 0h4v14h-4z");
+  //   }
+  // }
 
-  function inputsDisabled(isDisabled) {
-    allInputs.forEach((input) => {
-      input.disabled = isDisabled;
-    });
-  }
+  // function inputsDisabled(isDisabled) {
+  //   allInputs.forEach((input) => {
+  //     input.disabled = isDisabled;
+  //   });
+  // }
 
   start.addEventListener("click", () => {
     if (isRunning === true) {
